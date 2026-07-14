a09a7c61dac33f4e6b3001191f47dc07845a79aa
Providence7 <afuyeuthman2018@gmail.com>
2026-07-14 14:02:56 -0400

    sss

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,10 +1,19 @@
+// src/routes/ProtectedRoute.jsx
 import { Navigate, Outlet } from "react-router-dom";
 import { useAdminAuth } from "../context/AdminAuthContext.jsx";
 
 const ProtectedRoute = () => {
-  const { isLoggedIn } = useAdminAuth();
+  const { isLoggedIn, loading } = useAdminAuth();
 
-  return isLoggedIn ? <Outlet /> : <Navigate to="/login" replace />;
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center min-h-screen bg-[#FAF9F6] text-[#581845]">
+        Checking session...
+      </div>
+    );
+  }
+
+  return isLoggedIn ? <Outlet /> : <Navigate to="/admin/login" replace />;
 };
 
 export default ProtectedRoute;
